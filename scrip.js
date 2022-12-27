5cc780796559e2aa04a17a107cfa060d1b122771
Alan Almeida <alanalmeida8245@gmail.com>
2022-12-27 09:56:39 -0300

    Primeiro Commit

---
+++ scrip.js
@@ -0,0 +1,20 @@
+
+//navbar
+// Animação Maquina de Escrever
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
+//-------------------------------------//
+
+//Navbar
+
